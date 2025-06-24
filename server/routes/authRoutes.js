6f771c67602d191657d03770c2a30d2fa8b7932b
James Graham Featherston <jgfeatherston@meta.com>
2025-06-24 14:23:01 -0700

    Fixed auth route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,5 +5,6 @@ const authController = require("../contollers/authController");
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
+router.post("/me", authController.me);
 
 module.exports = router;
