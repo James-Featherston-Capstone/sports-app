12b57511e38f361c70af9f4c3e26f3e6a3647d7d
James Graham Featherston <jgfeatherston@meta.com>
2025-07-15 12:54:39 -0700

    Prefernce list APIs called with frontend

--- client/src/components/events-components/EventParkPreferences.tsx
+++ client/src/components/events-components/EventParkPreferences.tsx
@@ -1,26 +1,41 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
 import MapsInput from "../MapsInput";
+import type { ParkPreference } from "@/utils/interfaces";
+import { createEventPreference, upvotePreference } from "@/utils/eventService";
 
 interface EventParkPreferencesProps {
   eventId: number;
-  preferenceList: string[];
-  setPreferenceList: (preferences: string[]) => void;
+  preferenceList: ParkPreference[];
+  setPreferenceList: (preferences: ParkPreference[]) => void;
 }
 const EventParkPreferences = ({
   preferenceList,
   setPreferenceList,
+  eventId,
 }: EventParkPreferencesProps) => {
-  const [newPreference, setNewPreference] = useState("");
-  const handleNewParkPreference = () => {
+  const [newPreferenceLocation, setNewPreferenceLocation] = useState("");
+  const handleNewParkPreference = async () => {
+    const newPreference = await createEventPreference(
+      eventId,
+      newPreferenceLocation
+    );
     setPreferenceList([newPreference, ...preferenceList]);
+  };
+
+  const handleUpvote = async (preferenceId: number) => {
+    const updatedPreference = await upvotePreference(preferenceId);
+    const updatedPreferenceList = preferenceList.map((preference) =>
+      preference.id === preferenceId ? updatedPreference : preference
+    );
+    setPreferenceList(updatedPreferenceList);
   };
   return (
     <>
       <div className="flex flex-row flex-wrap">
         <MapsInput
-          location={newPreference}
-          setLocation={setNewPreference}
+          location={newPreferenceLocation}
+          setLocation={setNewPreferenceLocation}
           showMap={false}
         />
         <Button className="w-1/1 mx-0" onClick={handleNewParkPreference}>
@@ -28,7 +43,22 @@ const EventParkPreferences = ({
         </Button>
       </div>
       {preferenceList.map((preference) => {
-        return <h1>{preference}</h1>;
+        return (
+          <div className="bg-white rounded-sm my-0.5 p-0.5 flex">
+            <div className="grow-1">
+              <h1>{preference.location}</h1>
+            </div>
+            <div>
+              <p className="text-center">{preference.upvotes}</p>
+              <Button
+                className="p-0.25 h-auto "
+                onClick={() => handleUpvote(preference.id)}
+              >
+                Upvote
+              </Button>
+            </div>
+          </div>
+        );
       })}
     </>
   );
