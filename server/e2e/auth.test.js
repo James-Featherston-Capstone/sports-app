cf2f8b1c657b02f49ba11dc0ccd807e4cf17db02
James Graham Featherston <jgfeatherston@meta.com>
2025-07-23 10:01:44 -0700

    Playwright auth tests

---
+++ server/e2e/auth.test.js
@@ -0,0 +1,61 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test.describe("Authentication", async () => {
+  test("Login with new user", async ({ page }) => {
+    await page.goto("https://team-up-client.onrender.com/login");
+
+    await page.fill('input[placeholder="someone@gmail.com"]', "1@gmail.com");
+    await page.fill('input[placeholder="Password"]', "1");
+    await Promise.all([
+      page.click('button[type="submit"]'),
+      page.waitForURL("https://team-up-client.onrender.com"),
+    ]);
+
+    await expect(page.locator("text=Custom Filters")).toBeVisible();
+  });
+  test("Signup with new user", async ({ page }) => {
+    await page.goto("https://team-up-client.onrender.com/register");
+
+    const now = new Date().toISOString();
+    const formattedNow = now.replace(/[:.]/g, "-");
+
+    await page.fill('input[placeholder="Username"]', `${formattedNow}`);
+    await page.fill(
+      'input[placeholder="someone@gmail.com"]',
+      `${formattedNow}@gmail.com`
+    );
+
+    const passwordFields = page.locator('input[placeholder="Password"]');
+    await expect(passwordFields).toHaveCount(2);
+    await passwordFields.nth(0).fill("1");
+    await passwordFields.nth(1).fill("1");
+    await page.click('button[type="submit"]');
+    await page.waitForSelector('text="Create Profile"', {
+      state: "visible",
+    });
+    await page.fill('input[placeholder="Create a Bio"]', "Playwright test");
+    await page.waitForSelector('input[placeholder="Enter address..."]');
+    await page.fill('input[placeholder="Enter address..."]', "Canada");
+    await page.waitForSelector(".pac-container", {
+      state: "attached",
+      timeout: 10_000,
+    });
+    const firstField = page.locator(".pac-container .pac-item").first();
+    await firstField.waitFor({
+      state: "visible",
+      timeout: 10_000,
+    });
+    await page
+      .getByRole("textbox", { name: "Enter address..." })
+      .press("ArrowDown");
+    await page
+      .getByRole("textbox", { name: "Enter address..." })
+      .press("Enter");
+    await Promise.all([
+      page.waitForURL("https://team-up-client.onrender.com"),
+      page.click("text=Submit"),
+    ]);
+    await expect(page.locator("text=Custom Filters")).toBeVisible();
+  });
+});
