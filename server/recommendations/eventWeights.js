8a63c9643e2d0402e643ae0608227af28475851a
James Graham Featherston <jgfeatherston@meta.com>
2025-07-24 14:34:12 -0700

    Dynamic weights with stored data

--- server/recommendations/eventWeights.js
+++ server/recommendations/eventWeights.js
@@ -1,65 +1,46 @@
 const prisma = require("../prisma.js");
 
-/*
-Create a function to update the event counts:
-When you click on an event: Find the maxes for every feature that is non-linear
-
-Find the the new importance by of the each new count / totalCounts
-Create a max change variable by using a constant and doing
-for each weight weight = (1 - change) * w + change * new importance
-
-*/
 /**
  *
  * @param {*} click
  */
 const handleWeightChange = async (click) => {
-  await getRecommendationData(click.userId);
+  const recommendationData = await getRecommendationData(click.userId);
   const MAX_DAYS_AWAY = 7;
   const counts = {
-    timeOfDayCount: 0,
-    sportCount: 0,
-    dateCount: 0,
+    timeOfDayCount: parseFloat(recommendationData.timeOfDayCount),
+    sportCount: parseFloat(recommendationData.sportCount),
+    dateCount: parseFloat(recommendationData.dateCount),
   };
-  const mockWeights = {
-    timeOfDayWeight: 0.15,
-    sportWeight: 0.35,
-    dateWeight: 0.5,
+  const weights = {
+    timeOfDayWeight: recommendationData.timeOfDayWeight,
+    sportWeight: recommendationData.sportWeight,
+    dateWeight: recommendationData.dateWeight,
   };
 
-  const mockSportsBuckets = {
-    SOCCER: 5,
-    BASEBALL: 10,
-    BASKETBALL: 15,
-  };
-  const mockTimeOfDayBuckets = {
-    10: 5,
-    11: 2,
-    12: 7,
-  };
+  const sportBuckets = recommendationData.sportBuckets;
+  const timeOfDayBuckets = recommendationData.timeOfDayBuckets;
 
   const now = new Date();
   const eventTime = new Date(click.event.eventTime);
   const dateDiff = _getDaysDiff(eventTime, now);
   const timeOfDayBucket = _getTimeOfDayBucket(eventTime);
 
-  mockTimeOfDayBuckets[timeOfDayBucket] =
-    mockTimeOfDayBuckets[timeOfDayBucket] + 1 || 1;
-  mockSportsBuckets[click.event.sport] =
-    mockSportsBuckets[click.event.sport] + 1 || 1;
+  timeOfDayBuckets[timeOfDayBucket] =
+    timeOfDayBuckets[timeOfDayBucket] + 1 || 1;
+  sportBuckets[click.event.sport] = sportBuckets[click.event.sport] + 1 || 1;
 
-  const totalTimeOfDay = Object.values(mockTimeOfDayBuckets).reduce(
+  const totalTimeOfDay = Object.values(timeOfDayBuckets).reduce(
     (sum, sport) => sum + sport,
     0
   );
-  const totalSport = Object.values(mockSportsBuckets).reduce(
+  const totalSport = Object.values(sportBuckets).reduce(
     (sum, sport) => sum + sport,
     0
   );
 
-  const sportAddition = mockSportsBuckets[click.event.sport] / totalSport;
-  const timeOfDayAddition =
-    mockTimeOfDayBuckets[timeOfDayBucket] / totalTimeOfDay;
+  const sportAddition = sportBuckets[click.event.sport] / totalSport;
+  const timeOfDayAddition = timeOfDayBuckets[timeOfDayBucket] / totalTimeOfDay;
   const dateAddition =
     Math.max((MAX_DAYS_AWAY - dateDiff) / MAX_DAYS_AWAY, 0) * 0.33;
 
@@ -69,28 +50,53 @@ const handleWeightChange = async (click) => {
     dateCount: counts.dateCount + dateAddition,
   };
 
+  const total = Object.values(newCounts).reduce((sum, count) => sum + count, 1);
+
   const MAX_CHANGE = 0.1;
   const newTimeOfDayWeight = Math.min(
-    (1 - MAX_CHANGE) * mockWeights.timeOfDayWeight +
-      MAX_CHANGE * newCounts.timeOfDayCount,
+    (1 - MAX_CHANGE) * weights.timeOfDayWeight +
+      MAX_CHANGE * (newCounts.timeOfDayCount / total),
     0.15
   );
-  const newDateWeight =
-    (1 - MAX_CHANGE) * mockWeights.dateWeight +
-    MAX_CHANGE * newCounts.dateCount;
-  const newSportWeight =
-    (1 - MAX_CHANGE) * mockWeights.sportWeight +
-    MAX_CHANGE * newCounts.sportCount;
+  const newDateWeight = Math.min(
+    (1 - MAX_CHANGE) * weights.dateWeight +
+      MAX_CHANGE * (newCounts.dateCount / total),
+    0.65
+  );
+  const newSportWeight = Math.min(
+    (1 - MAX_CHANGE) * weights.sportWeight +
+      MAX_CHANGE * (newCounts.sportCount / total),
+    0.65
+  );
+
+  const normalized = _normalizeWeights(
+    newTimeOfDayWeight,
+    newDateWeight,
+    newSportWeight
+  );
 
   const updatedRecommendationData = {
     ...newCounts,
-    timeOfDayBuckets: mockTimeOfDayBuckets,
-    sportBuckets: mockSportsBuckets,
-    sportWeight: newSportWeight,
-    dateWeight: newDateWeight,
-    timeOfDayWeight: newTimeOfDayWeight,
+    timeOfDayBuckets: timeOfDayBuckets,
+    sportBuckets: sportBuckets,
+    sportWeight: normalized.sportWeight,
+    dateWeight: normalized.dateWeight,
+    timeOfDayWeight: normalized.timeOfDayWeight,
   };
   updateRecommendationData(click.userId, updatedRecommendationData);
+};
+
+const _normalizeWeights = (
+  newTimeOfDayWeight,
+  newDateWeight,
+  newSportWeight
+) => {
+  const total = newTimeOfDayWeight + newDateWeight + newSportWeight;
+  return {
+    timeOfDayWeight: newTimeOfDayWeight / total,
+    dateWeight: newDateWeight / total,
+    sportWeight: newSportWeight / total,
+  };
 };
 
 const _getDaysDiff = (date1, date2) => {
@@ -115,7 +121,7 @@ const getRecommendationData = async (userId) => {
     update: {},
     create: { userId: userId, timeOfDayBuckets: {}, sportBuckets: {} },
   });
-  console.log(data);
+  return data;
 };
 
 /* 
@@ -126,6 +132,5 @@ const updateRecommendationData = async (userId, newData) => {
     where: { userId: userId },
     data: newData,
   });
-  console.log(result);
 };
 module.exports = { handleWeightChange };
