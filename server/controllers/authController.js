66e8997a822b9ab9339904824b9f8ec12bb78938
James Graham Featherston <jgfeatherston@meta.com>
2025-07-25 15:28:24 -0700

    Remove unnecessary try catch

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,96 +9,76 @@ const {
 const { buildProfile } = require("../utils/buildModel");
 const locationUtils = require("../recommendations/locationUtils.js");
 
-exports.register = async (req, res, next) => {
-  try {
-    const { email, password: plainPassword, username } = req.body;
-    if (!email || !plainPassword || !username) {
-      throw new ValidationError("Missing email or password");
-    }
-    const filters = { email: email };
-    const existingUser = await authService.getUser(filters);
-    if (existingUser) {
-      throw new ConflictError("This email is already in use");
-    }
-    const profile = buildProfile(req);
-    const hash = await hashPassword(plainPassword);
-    const userCredentials = { email, password: hash, username: username };
-    const newUser = { ...profile, ...userCredentials };
-    await locationUtils.extractLatLngFields(newUser);
-    if (
-      !newUser.bio ||
-      !newUser.location ||
-      !newUser.latitude ||
-      !newUser.longitude ||
-      !newUser.latitudeKey ||
-      !newUser.longitudeKey
-    ) {
-      throw new ValidationError("Location and bio required");
-    }
-    const retUser = await authService.createUser(newUser);
-    req.session.user = retUser;
-    res.json(retUser);
-  } catch (error) {
-    next(error);
+exports.register = async (req, res) => {
+  const { email, password: plainPassword, username } = req.body;
+  if (!email || !plainPassword || !username) {
+    throw new ValidationError("Missing email or password");
+  }
+  const filters = { email: email };
+  const existingUser = await authService.getUser(filters);
+  if (existingUser) {
+    throw new ConflictError("This email is already in use");
+  }
+  const profile = buildProfile(req);
+  const hash = await hashPassword(plainPassword);
+  const userCredentials = { email, password: hash, username: username };
+  const newUser = { ...profile, ...userCredentials };
+  await locationUtils.extractLatLngFields(newUser);
+  if (
+    !newUser.bio ||
+    !newUser.location ||
+    !newUser.latitude ||
+    !newUser.longitude ||
+    !newUser.latitudeKey ||
+    !newUser.longitudeKey
+  ) {
+    throw new ValidationError("Location and bio required");
+  }
+  const retUser = await authService.createUser(newUser);
+  req.session.user = retUser;
+  res.json(retUser);
+};
+
+exports.checkEmail = async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    throw new ValidationError("Missing email");
+  }
+  const filters = { email: email };
+  const existingUser = await authService.getUser(filters);
+  if (existingUser) {
+    res.json({ emailInUse: true });
+  } else {
+    res.json({ emailInUse: false });
   }
 };
 
-exports.checkEmail = async (req, res, next) => {
-  try {
-    const { email } = req.body;
-    if (!email) {
-      throw new ValidationError("Missing email");
-    }
-    const filters = { email: email };
-    const existingUser = await authService.getUser(filters);
-    if (existingUser) {
-      res.json({ emailInUse: true });
-    } else {
-      res.json({ emailInUse: false });
-    }
-  } catch (error) {
-    next(error);
+exports.login = async (req, res) => {
+  const { email, password: plainPassword } = req.body;
+  if (!email || !plainPassword) {
+    throw new ValidationError("Missing email or password");
   }
-};
-
-exports.login = async (req, res, next) => {
-  try {
-    const { email, password: plainPassword } = req.body;
-    if (!email || !plainPassword) {
-      throw new ValidationError("Missing email or password");
-    }
-    const filters = { email: email };
-    const user = await authService.getUser(filters);
-    if (user && (await verifyPassword(plainPassword, user.password))) {
-      req.session.user = user;
-      res.json(user);
-    } else {
-      throw new NotFoundError("Email or password is incorrect.");
-    }
-  } catch (error) {
-    next(error);
-  }
-};
-
-exports.logout = async (req, res, next) => {
-  try {
-    req.session.destroy(() => {
-      res.json({ message: "Logout successful" });
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-exports.me = async (req, res, next) => {
-  try {
-    if (!req.session.user) {
-      throw new UnauthorizedError("User is not logged in");
-    }
-    const filters = { id: req.session.user.id };
-    const user = await authService.getUser(filters);
+  const filters = { email: email };
+  const user = await authService.getUser(filters);
+  if (user && (await verifyPassword(plainPassword, user.password))) {
+    req.session.user = user;
     res.json(user);
-  } catch (error) {
-    next(error);
+  } else {
+    throw new NotFoundError("Email or password is incorrect.");
   }
+};
+
+exports.logout = async (req, res) => {
+  req.session.destroy(() => {
+    res.json({ message: "Logout successful" });
+  });
+};
+
+exports.me = async (req, res) => {
+  if (!req.session.user) {
+    throw new UnauthorizedError("User is not logged in");
+  }
+  const filters = { id: req.session.user.id };
+  const user = await authService.getUser(filters);
+  res.json(user);
 };
