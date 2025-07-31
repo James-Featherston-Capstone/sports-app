e248ea99324ea23954369273f23d38e1a994fdae
James Graham Featherston <jgfeatherston@meta.com>
2025-07-31 13:56:07 -0700

    Final changes

--- client/src/components/events-components/CommentBox.tsx
+++ client/src/components/events-components/CommentBox.tsx
@@ -6,7 +6,7 @@ interface CommentBoxProps {
 
 const CommentBox = ({ comment }: CommentBoxProps) => {
   return (
-    <div className="w-1/1 bg-white rounded-sm py-1 px-3 my-1">
+    <div className="w-1/1 bg-white rounded-sm py-1 px-3 my-1 border-2">
       <div>
         <p className="text-[10px]">{comment.author.username}</p>
         <p className="text-[12px]">{comment.comment}</p>
